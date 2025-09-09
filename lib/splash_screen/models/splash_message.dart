306820f634354cb2e6843ae7429c309d84a37b9c
bautistageng <gengbautista1234@gmail.com>
2025-09-09 19:23:15 +0800

    splash screen updates

--- lib/splash_screen/models/splash_message.dart
+++ lib/splash_screen/models/splash_message.dart
@@ -6,27 +6,27 @@ class OnBoards {
 
 List<OnBoards> onBoardData = [
   OnBoards(
-    text: "Welcome to Calmora! Where mental health meets trusted care.",
+    text: "This is Calmora! Where mental health meets trusted care.",
     image: "images/calmora_circle_crop.png",
   ),
   // OnBoards(
   //   text: "It's okay to feel broken sometimes. Showing your vulnerable side is not a sign of weakness it's a step toward true strength and healing.",
   //   image: "images/splash/image5.png",
   // ),
-  // OnBoards(
-  //   text: "Life can feel overwhelming, like noise that won't quiet down. Here, you'll find tools to untangle your thoughts and move toward clarity and peace.",
-  //   image: "images/splash/image7.png",
-  // ),
   OnBoards(
-    text: "Discover Resources Access articles, tools, and tips to help manage stress, anxiety, and mental health challenges.",
-    image: "images/splash/image1.png",
+    text: "Discover Resources, including articles, tools, and tips to help manage stress, anxiety, and other mental health challenges.",
+    image: "images/splash/resources.png",
+  ),
+    OnBoards(
+    text: "Talk to our chatbot to assist you and guide you through your needs.",
+    image: "images/splash/chatbot.png",
   ),
   OnBoards(
-    text: "Connect with Specialists, Find licensed therapists, and mental health experts to guide you through your journey.",
-    image: "images/splash/image2.png",
+    text: "Find licensed therapists, and mental health experts to guide you through your journey.",
+    image: "images/splash/specialist.png",
   ),
   OnBoards(
-    text: "Find the right help! Chat with specialist and share experiences in a safe space for men.",
-    image: "images/splash/image3.png",
+    text: "Begin your journey to a healthier mental health now and achive the peace you've always desserve.",
+    image: "images/splash/peace.png",
   ),
 ];
